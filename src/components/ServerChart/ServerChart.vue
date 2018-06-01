648030e2287dc02a93e34bdb6e6aacacd783397c
Layne Cao(曹雷) <caolei@inspur.com>
2018-06-01 16:33:48 +0800

    图表验证

--- src/components/ServerChart/ServerChart.vue
+++ src/components/ServerChart/ServerChart.vue
@@ -1,16 +1,50 @@
 <template>
-  <div></div>
+<chart :options="option" theme="light" ></chart>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-    };
-  },
+import ECharts from 'vue-echarts/components/ECharts.vue'
+import 'echarts/lib/chart/bar'
+import 'echarts/lib/chart/line'
+import 'echarts/lib/chart/pie'
+import 'echarts/lib/chart/map'
+import 'echarts/lib/chart/radar'
+import 'echarts/lib/chart/scatter'
+import 'echarts/lib/chart/effectScatter'
+import 'echarts/lib/component/tooltip'
+import 'echarts/lib/component/polar'
+import 'echarts/lib/component/geo'
+import 'echarts/lib/component/legend'
+import 'echarts/lib/component/title'
+import 'echarts/lib/component/visualMap'
+import 'echarts/lib/component/dataset'
 
-  components: {}
+export default {
+  components: {
+    chart: ECharts
+  },
+  data: function () {
+    return {
+      option:{
+                xAxis: {
+                    type: 'category',
+                    // data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: [{
+                    // data: [120, 200, 150, 80, 70, 110, 130],
+                    type: 'bar'
+                }]
+            }
+    }
+  }
 }
 </script>
+
 <style>
+.echarts {
+  height: 300px;
+}
 </style>
