41b52c792b78d5ad8aa2ab7e0bfc3a605819b8c8
Layne Cao(曹雷) <caolei@inspur.com>
2018-06-06 17:42:41 +0800

    日期组件

--- src/components/filtertools/datetool.vue
+++ src/components/filtertools/datetool.vue
@@ -1,50 +1,44 @@
 <template>
-  <el-col v-if="item" :span="toolSize">
+  <el-col  :span="toolSize">
     <div class="grid-content">
-      <el-form-item :label="item.title" :prop="item.id">
+      <el-form-item :label="param.title" :prop="param.id">
         <el-date-picker
-          v-model="myrulename" :disabled="item.readonly"
-          type="date"
-          placeholder="选择日期"
-          value-format="yyyy-MM-dd">
+            v-model.lazy="internalValue" 
+            @change="$emit('rule-form-change',param.id,internalValue)"
+            :disabled="param.readonly"
+            :type="getType()"
+            :value-format="getValueFormat()">
         </el-date-picker>
-      </el-form-item>        
+      </el-form-item>
     </div>
   </el-col>
 </template>
 
 <script>
 export default {
-  props:{
-    ruleForm:{
-      type: Object
-    },
-    item:{
-      type: Object
-    },
-    toolSize:{
-    
-    }
-  },
+  props:['param','toolSize','ruleFormValue'],
   data () {
     return {
-      myrulename:this.item.defaultValue   
+       internalValue : this.param.defaultValue
     };
   },
-  watch:{
-    myrulename:function(val,oldval){  
-      if(val){
-        var computedVal = val.substring(0,4)+val.substring(5,7)+val.substring(8)
-      }else{
-        var computedVal = val
-      }     
-      this.$set(this.item,'defaultValue',computedVal)
-      this.$set(this.ruleForm,this.item.id,val)
-     // this.$emit('update:this.ruleForm', [this.item.id,val],)
-      this.$emit("on-result-change",this.item.id)
+  methods:{
+    getType(){
+      if(this.param.contentType=="Y")
+        return "year";
+      else if(this.param.contentType=="D")
+        return "date";
+      else 
+        return "month";
+    },
+    getValueFormat(){
+      if(this.param.contentType=="Y")
+        return "yyyy";
+      else if(this.param.contentType=="D")
+        return "yyyyMMdd";
+      else 
+        return "yyyyMM";
     }
-  },
-  created(){
   }
 }
 </script>
