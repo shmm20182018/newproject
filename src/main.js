a64f1e2d5d6765d81c4fda7e494cf2fa27eb94fe
Layne Cao(曹雷) <caolei@inspur.com>
2018-06-08 14:05:47 +0800

    packge

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import deepCloneArray from './utils/deepClone.js'
 import http from './utils/http.js'
-import $ from 'jquery'
+// import $ from 'jquery'
+import App from './App'
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false
 Vue.prototype.$Http= http
 Vue.prototype.$Clone = deepCloneArray
-Vue.prototype.$Extend =$.extend
+// Vue.prototype.$Extend =$.extend
 
 Vue.directive('drag',//自定义指令                                 
         {bind:function (el, binding) {
